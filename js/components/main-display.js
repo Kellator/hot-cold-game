32201865a1acdeb682a0755c5383aed95c7d6336
Kellator <k.macleod21@gmail.com>
2017-02-23 21:42:24 -0500

    actions moved to correct js file.  provider component added to index.js

--- js/components/main-display.js
+++ js/components/main-display.js
@@ -3,3 +3,10 @@ import {connect} from 'react-redux';
 
 import GuessBlock from 'guess-block';
 import * as actions from '../actions/index';
+
+export class MainDisplay extends React.Component {
+	constructor(props) {
+		super(props);
+		
+	}
+}
--- js/index.js
+++ js/index.js
@@ -1,46 +1,17 @@
 require('babel-polyfill');
 
-// display initial state of game (new game)
-export const NEW_GAME = 'NEW_GAME';
-export const newGame = game => ({
-	type: NEW_GAME,
-	game
-});
-// generate a random number
-export const GENERATE_NUMBER = 'GENERATE_NUMBER';
-export const generateNumber = answer => ({
-	type: GENERATE_NUMBER,
-	answer //function to generate number?
-});
-// submit guess
-export const SUBMIT_GUESS = 'SUBMIT_GUESS';
-export const submitGuess = submitGuess => ({
-	type: SUBMIT_GUESS,
-	submitGuess
-});
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
 
-// input guess
-// export const INPUT_GUESS = 'INPUT_GUESS';
-// export const inputGuess = inputGuess => ({
-// 	type: INPUT_GUESS,
-// 	inputGuess
-// });
-// compare guess with random number
-// export const COMPARE_GUESS = 'COMPARE_GUESS';
-// export const compareGuess = compare => ({
-// 	type: COMPARE_GUESS,
-// 	compare
-// });
-// display message
-// export const DISPLAY_GUESS = 'DISPLAY_GUESS';
-// export const displayGuess = display => ({
-// 	type: DISPLAY_GUESS,
-// 	display
-// });
-// determine if guess is hot or cold
-// export const HOT_COLD = 'HOT_COLD';
-// export const hotCold = temperature => ({
-// 	type: HOT_COLD,
-// 	temperature
-// });
-// start new game
+import store from './store';
+import MainDisplay from './components/main-display';
+
+document.addEventListener('DOMContentLoaded', () =>
+	ReactDOM.render (
+		<Provider store={store}>
+			<GameDisplay />
+		</Provider>,
+		document.getElementById('app')
+	)
+);
